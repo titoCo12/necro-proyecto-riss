b36a7bc59eba4c6dbf9f744e59c67384968e316a
Fuentes, Joaquín <joaquinfuentes170@gmail.com>
2025-07-10 16:14:42 -0300

    caminar derecha izquierda

--- scripts/necro.gd
+++ scripts/necro.gd
@@ -1,28 +1,49 @@
 extends CharacterBody2D
 
+const RIGHT = 'R'
+const LEFT = 'L'
+const DOWN = 'D'
+const UP = 'U'
+
+
 var speed = 100
+var last_dir = RIGHT
+
 
 func _physics_process(delta):
-	move_input(delta)
+	walk_input(delta)
 
-func move_input(delta):
+
+func walk_input(delta):
+	
+	velocity.y = 0
+	velocity.x = 0
+	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-		velocity.y = 0
+		velocity.x = speed		
+		last_dir = RIGHT
 		
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
-		velocity.y = 0
+		last_dir = LEFT
 		
 	elif Input.is_action_pressed("ui_down"):
-		velocity.x = 0
 		velocity.y = speed
+		last_dir = DOWN
 		
 	elif Input.is_action_pressed("ui_up"):
-		velocity.x = 0
 		velocity.y = -speed
-	else:
-		velocity.x = 0
-		velocity.y = 0
-
+		last_dir = UP
+	
+	walk_animation(velocity.y + velocity.x)
 	move_and_slide()
+
+
+func walk_animation(speed):
+	var animation = $AnimatedSprite2D
+	
+	if speed:
+		animation.play("Walking-" + last_dir)
+	else:
+		animation.play("Idle-" + last_dir)	
+
