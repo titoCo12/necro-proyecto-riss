a76f142e9fa23ee66acd9e75915b5b319c34a135
Fuentes, Joaquín <joaquinfuentes170@gmail.com>
2025-07-12 21:30:12 -0300

    base isometrica

--- scripts/necro.gd
+++ scripts/necro.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-const RIGHT = 'R'
-const LEFT = 'L'
-const DOWN = 'D'
-const UP = 'U'
+const RIGHT = 'D'
+const LEFT = 'A'
+const DOWN = 'S'
+const UP = 'W'
 
 
 var speed = 100
@@ -19,23 +19,24 @@ func walk_input(delta):
 	velocity.y = 0
 	velocity.x = 0
 	
+	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed		
+		velocity.x += speed		
 		last_dir = RIGHT
 		
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
 		last_dir = LEFT
 		
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += speed
 		last_dir = DOWN
 		
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -speed
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= speed
 		last_dir = UP
 	
-	walk_animation(velocity.y + velocity.x)
+	#walk_animation(velocity.y + velocity.x)
 	move_and_slide()
 
 
